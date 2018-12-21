fc19c6cc3d3f8a3e469064a1ba0f4ad84dc38f7f
Mike G <michael.goldsmith@datalex.com>
2018-12-21 16:48:48 +0000

    Issue-04 - added focus manager and coded the receiver... TODO: register and unregister receiver

---
+++ app/src/main/java/com/example/mike/mp3player/service/AudioFocusManager.java
@@ -0,0 +1,107 @@
+package com.example.mike.mp3player.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+import androidx.media.AudioAttributesCompat;
+import androidx.media.AudioFocusRequestCompat;
+import androidx.media.AudioManagerCompat;
+
+public class AudioFocusManager extends BroadcastReceiver
+ implements AudioManager.OnAudioFocusChangeListener {
+
+    public static final float MEDIA_VOLUME_DEFAULT = 1.0f;
+    private static final float MEDIA_VOLUME_DUCK = 0.2f;
+
+    MyMediaPlayerAdapter player;
+    AudioManager audioManager;
+    Context context;
+
+    boolean playWhenAudioFocusGained = false;
+
+    public AudioFocusManager(Context context, MyMediaPlayerAdapter player) {
+        this.context = context;
+        this.player = player;
+    }
+
+    public void init() {
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @Override
+    public void onAudioFocusChange(int focusChange) {
+        switch (focusChange) {
+            case AudioManager.AUDIOFOCUS_GAIN:
+                if (playWhenAudioFocusGained && !player.isPlaying()) {
+                    player.play();
+                } else if (player.isPlaying()) {
+                    player.setVolume(MEDIA_VOLUME_DEFAULT);
+                }
+                playWhenAudioFocusGained = false;
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                player.setVolume(MEDIA_VOLUME_DUCK);
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                if (player.isPlaying()) {
+                    playWhenAudioFocusGained = true;
+                    player.pause();
+                }
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS:
+                abandonAudioFocus();
+                playWhenAudioFocusGained  = false;
+                player.pause();
+                break;
+        }
+    }
+
+    public boolean requestAudioFocus() {
+        AudioAttributesCompat.Builder audioAttributesBuilder = new AudioAttributesCompat.Builder()
+                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                .setUsage(AudioAttributesCompat.USAGE_MEDIA)
+                .setContentType(AudioAttributesCompat.CONTENT_TYPE_MUSIC);
+        AudioAttributesCompat audioAttributes = audioAttributesBuilder.build();
+
+        AudioFocusRequestCompat audioFocusRequestCompat = new AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(audioAttributes)
+                .setOnAudioFocusChangeListener(this)
+                .setWillPauseWhenDucked(false).build();
+
+        int result = AudioManagerCompat.requestAudioFocus(audioManager, audioFocusRequestCompat);
+
+        return isRequestGranted(result);
+    }
+
+    private boolean isRequestGranted(int result) {
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    public void playbackPaused() {
+        if (!playWhenAudioFocusGained) {
+            abandonAudioFocus();
+        }
+    }
+
+    private boolean abandonAudioFocus() {
+        AudioFocusRequestCompat audioFocusRequestCompat = new AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN)
+                .setOnAudioFocusChangeListener(this)
+                .build();
+        int result = AudioManagerCompat.abandonAudioFocusRequest(audioManager, audioFocusRequestCompat);
+        return  isRequestGranted(result);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
+            if (player.isPlaying()) {
+                player.pause();
+            }
+        }
+    }
+}
