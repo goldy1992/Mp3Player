717c32e9c53f4178dd5273b58790b579ad3a5e6f
Mike G <michael.goldsmith@datalex.com>
2019-06-11 13:36:16 +0100

    Issue-52 - removed redundant classes and added IntentUtilsTest

--- app/src/main/java/com/example/mike/mp3player/client/utils/IntentUtils.java
+++ app/src/main/java/com/example/mike/mp3player/client/utils/IntentUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.media.session.MediaSessionCompat;
 
+import androidx.annotation.NonNull;
+
 import com.example.mike.mp3player.client.activities.MediaPlayerActivity;
 import com.example.mike.mp3player.commons.library.LibraryRequest;
 
@@ -15,15 +17,13 @@ import static com.example.mike.mp3player.commons.Constants.REQUEST_OBJECT;
  * Utility classes for making intent objects for other classes and services
  */
 public final class IntentUtils {
-
-    public static Intent createMediaPlayerActivityMediaRequestIntent(Context context, MediaSessionCompat.Token token, String songId, LibraryRequest parentId) {
-        Intent intent = createGoToMediaPlayerActivity(context, token);
-        intent.putExtra(MEDIA_ID, songId);
-        intent.putExtra(REQUEST_OBJECT, parentId);
-        return intent;
-    }
-
-    public static Intent createGoToMediaPlayerActivity(Context context, MediaSessionCompat.Token token) {
+    /**
+     * Creates an intent to go to the MediaPlayerActivity
+     * @param context context
+     * @param token the media session token
+     * @return an Intent to go to the MediaPlayerActivity
+     */
+    public static Intent createGoToMediaPlayerActivity(@NonNull Context context, @NonNull MediaSessionCompat.Token token) {
         Intent intent = new Intent(context, MediaPlayerActivity.class);
         intent.putExtra(MEDIA_SESSION, token);
         return intent;
