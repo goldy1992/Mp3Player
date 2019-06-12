a1d546d36ec679807fcdca9e93b50ba0244a2b6c
Mike G <michael.goldsmith@datalex.com>
2019-06-12 18:18:52 +0100

    Issue-52 - TODO: refactor song adapter test

--- app/src/test/java/com/example/mike/mp3player/TestUtils.java
+++ app/src/test/java/com/example/mike/mp3player/TestUtils.java
@@ -1,9 +1,13 @@
 package com.example.mike.mp3player;
 
+import android.os.Bundle;
 import android.support.v4.media.MediaBrowserCompat.MediaItem;
 import android.support.v4.media.MediaDescriptionCompat;
 
 import com.example.mike.mp3player.commons.library.Category;
+
+import static com.example.mike.mp3player.commons.MetaDataKeys.STRING_METADATA_KEY_ARTIST;
+import static com.example.mike.mp3player.commons.MetaDataKeys.STRING_METADATA_KEY_DURATION;
 
 public final class TestUtils {
     private TestUtils(){}
@@ -39,10 +43,22 @@ public final class TestUtils {
      * @return a new MediaItem
      */
     public static MediaItem createMediaItem(final String id, final String title, final String description) {
+        return createMediaItem(id, title, description, null);
+    }
+
+    public static MediaItem createMediaItem(final String id, final String title, final String description, final String duration) {
+        return createMediaItem(id, title, description, duration, null);
+    }
+
+    public static MediaItem createMediaItem(final String id, final String title, final String description, final String duration, String artist) {
+        Bundle bundle = new Bundle();
+        bundle.putString(STRING_METADATA_KEY_DURATION, duration);
+        bundle.putString(STRING_METADATA_KEY_ARTIST, artist);
         MediaDescriptionCompat mediaDescriptionCompat = new MediaDescriptionCompat.Builder()
                 .setDescription(description)
                 .setTitle(title)
                 .setMediaId(id)
+                .setExtras(bundle)
                 .build();
         return new MediaItem(mediaDescriptionCompat, 0);
     }
