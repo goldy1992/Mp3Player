0dc4d96484e8206f09f59491cfbd99c8fc63453f
Mike <goldy13_1992@hotmail.co.uk>
2019-06-14 23:35:09 +0100

    Issue-52 - started to work on MediaPlayerPool test

--- app/src/test/java/com/example/mike/mp3player/service/player/MediaPlayerPoolTest.java
+++ app/src/test/java/com/example/mike/mp3player/service/player/MediaPlayerPoolTest.java
@@ -1,29 +1,53 @@
 package com.example.mike.mp3player.service.player;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Looper;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowMediaPlayer;
+import org.robolectric.shadows.util.DataSource;
 
+@RunWith(RobolectricTestRunner.class)
 public class MediaPlayerPoolTest {
 
-    @Mock
+
     private Context context;
     @Mock
     private Looper looper;
+    @Mock
+    private Uri uri;
 
     private MediaPlayerPool m_mediaPlayerPool;
-    @BeforeEach
-    void setUp() {
+
+    @Before
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
+        this.context = InstrumentationRegistry.getInstrumentation().getContext();
+        final String path = "dummy";
+        Uri uri = new Uri.Builder().appendPath(path).build();
+        ShadowMediaPlayer.addMediaInfo(
+                DataSource.toDataSource(context, uri),
+                new ShadowMediaPlayer.MediaInfo(100, 10));
+
         m_mediaPlayerPool = new MediaPlayerPool(context);
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    public void testReset() {
+        m_mediaPlayerPool.reset(uri);
+    }
+
+    @After
+    public void tearDown() {
     }
 
 }
