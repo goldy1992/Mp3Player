ca1b1f9c2d987570eba79bfc2ca7f2ca66b5f29f
Mike G <michael.goldsmith@datalex.com>
2019-06-22 13:11:13 +0100

    Issue-57 - committing latest code in the decoupling of the service classes

--- app/src/main/java/com/example/mike/mp3player/dagger/components/ServiceComponent.java
+++ app/src/main/java/com/example/mike/mp3player/dagger/components/ServiceComponent.java
@@ -1,24 +1,42 @@
 package com.example.mike.mp3player.dagger.components;
 
+import android.support.v4.media.session.MediaSessionCompat;
+
 import com.example.mike.mp3player.dagger.modules.service.MediaLibraryModule;
 import com.example.mike.mp3player.dagger.modules.service.MediaPlayerAdapterModule;
 import com.example.mike.mp3player.dagger.modules.service.MediaRetrieverModule;
 import com.example.mike.mp3player.dagger.modules.service.MediaSessionCallbackModule;
+import com.example.mike.mp3player.dagger.modules.service.ServiceContextModule;
+import com.example.mike.mp3player.dagger.modules.service.ServiceModule;
 import com.example.mike.mp3player.service.MediaPlaybackService;
+import com.example.mike.mp3player.service.MyNotificationManager;
+import com.example.mike.mp3player.service.ServiceManager;
 import com.example.mike.mp3player.service.library.MediaLibrary;
 import com.example.mike.mp3player.service.library.mediaretriever.MediaRetriever;
+import com.example.mike.mp3player.service.session.MediaSessionAdapter;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
 
 @Singleton
-@Component(modules = {MediaLibraryModule.class, MediaRetrieverModule.class,
-        MediaPlayerAdapterModule.class, MediaSessionCallbackModule.class})
+@Component(modules = {
+        MediaLibraryModule.class,
+        MediaRetrieverModule.class,
+        MediaPlayerAdapterModule.class,
+        MediaSessionCallbackModule.class,
+        ServiceModule.class,
+        ServiceContextModule.class})
 public interface ServiceComponent {
 
+    MediaPlaybackService provideMediaPlaybackService();
     MediaRetriever mediaRetriever();
     MediaLibrary provideMediaLibrary();
+    ServiceManager provideServiceManager();
+    MyNotificationManager provideMyNotificationManager();
+    MediaSessionCompat provideMediaSessiomCompat();
+    MediaSessionCompat.Token provideMediaSessionToken();
+    MediaSessionAdapter mediaSessionAdapter();
 
     void inject(MediaPlaybackService mediaPlaybackService);
     void injectMediaRetriever(MediaLibrary mediaLibrary);
