9d499285206406daa9062b5d7a52a147265a7e37
Mike Goldsmith <michael.goldsmith@datalex.com>
2019-07-24 18:16:17 +0100

    Issue-71 - TODO: use linear layout weight to solve display problems

--- app/src/main/java/com/example/mike/mp3player/client/AlbumArtPainter.java
+++ app/src/main/java/com/example/mike/mp3player/client/AlbumArtPainter.java
@@ -1,10 +1,17 @@
 package com.example.mike.mp3player.client;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.FileNotFoundException;
 
 import javax.inject.Inject;
 
@@ -18,6 +25,17 @@ public class AlbumArtPainter {
     }
 
     public void paintOnView(ImageView imageView, Uri uri) {
-        Glide.with(context).load(uri).centerCrop().into(imageView);
+        RequestManager requestManager = Glide.with(context);
+        RequestOptions requestOptions = new RequestOptions();
+        /* TODO: add an error drawable image for when the album art is not found:
+        requestOptions.error()
+        */
+
+        try {
+            RequestBuilder<Drawable> drawableRequestBuilder = requestManager.load(uri);
+            drawableRequestBuilder.centerCrop().into(imageView);
+        } catch (Exception ex) {
+            // TODO: load a default image when the album art if not found
+        }
     }
 }
---
+++ app/src/main/java/com/example/mike/mp3player/client/views/SquareImageView.java
@@ -0,0 +1,46 @@
+package com.example.mike.mp3player.client.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.mike.mp3player.R;
+
+public class SquareImageView extends AppCompatImageView {
+
+    private static final int USE_WIDTH = 0;
+    private static final int USE_HEIGHT = 1;
+
+    private final int useWidthOrHeight;
+
+    public SquareImageView(Context context) {
+        this(context, null);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.useWidthOrHeight = attrs.getAttributeResourceValue(R.attr.useWidthOrHeight, USE_WIDTH);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int squareLength = 0;
+        switch (useWidthOrHeight) {
+            case USE_WIDTH:
+                squareLength = getMeasuredWidth();
+                setMeasuredDimension(squareLength, squareLength);
+                break;
+            case USE_HEIGHT:
+                squareLength = getMeasuredHeight();
+                setMeasuredDimension(squareLength, squareLength);
+                break;
+            default:setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight());
+        }
+    }
+}
