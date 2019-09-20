9e8815168d396380e577940467ebc9490e1b1022
Mike Goldsmith <michael.goldsmith@datalex.com>
2019-09-20 18:37:09 +0100

    Issue-90 - removed redundant files

--- app/src/main/java/com/example/mike/mp3player/dagger/modules/service/MediaSessionCompatModule.java
+++ app/src/main/java/com/example/mike/mp3player/dagger/modules/service/MediaSessionCompatModule.java
@@ -3,12 +3,9 @@ package com.example.mike.mp3player.dagger.modules.service;
 import android.content.Context;
 import android.support.v4.media.session.MediaSessionCompat;
 
-import com.example.mike.mp3player.service.PlaybackManager;
 import com.example.mike.mp3player.service.player.DecreaseSpeedProvider;
 import com.example.mike.mp3player.service.player.IncreaseSpeedProvider;
-import com.example.mike.mp3player.service.player.MediaPlayerAdapter;
 import com.example.mike.mp3player.service.player.MyMediaButtonEventHandler;
-import com.example.mike.mp3player.service.session.MediaSessionAdapter;
 
 import javax.inject.Singleton;
 
@@ -30,13 +27,6 @@ public class MediaSessionCompatModule {
     @Provides
     public MediaSessionCompat.Token provideMediaSessionToken(MediaSessionCompat mediaSessionCompat) {
         return mediaSessionCompat.getSessionToken();
-    }
-
-    @Singleton
-    @Provides
-    public MediaSessionAdapter mediaSessionAdapter(MediaSessionCompat mediaSession,
-                                   PlaybackManager playbackManager, MediaPlayerAdapter mediaPlayerAdapter) {
-        return new MediaSessionAdapter(mediaSession, playbackManager, mediaPlayerAdapter);
     }
 
     @Provides
--- app/src/test/java/com/example/mike/mp3player/dagger/components/TestServiceComponent.java
+++ app/src/test/java/com/example/mike/mp3player/dagger/components/TestServiceComponent.java
@@ -3,16 +3,12 @@ package com.example.mike.mp3player.dagger.components;
 import com.example.mike.mp3player.dagger.modules.MockContentManagerModule;
 import com.example.mike.mp3player.dagger.modules.MockMediaSessionConnectorModule;
 import com.example.mike.mp3player.dagger.modules.PlaybackNotificationManagerModule;
-import com.example.mike.mp3player.dagger.modules.service.ContentManagerModule;
 import com.example.mike.mp3player.dagger.modules.service.ContentRetrieverModule;
 import com.example.mike.mp3player.dagger.modules.service.ContentSearcherModule;
 import com.example.mike.mp3player.dagger.modules.service.ExoPlayerModule;
 import com.example.mike.mp3player.dagger.modules.service.HandlerThreadModule;
 import com.example.mike.mp3player.dagger.modules.service.MediaItemTypeIdModule;
-import com.example.mike.mp3player.dagger.modules.service.MediaPlayerAdapterModule;
-import com.example.mike.mp3player.dagger.modules.service.MediaSessionCallbackModule;
 import com.example.mike.mp3player.dagger.modules.service.MediaSessionCompatModule;
-import com.example.mike.mp3player.dagger.modules.service.MediaSessionConnectorModule;
 import com.example.mike.mp3player.dagger.modules.service.PlaybackManagerModule;
 import com.example.mike.mp3player.dagger.modules.service.ServiceModule;
 
@@ -28,7 +24,6 @@ import dagger.Component;
         ExoPlayerModule.class,
         HandlerThreadModule.class,
         MediaItemTypeIdModule.class,
-        MediaPlayerAdapterModule.class,
         MockMediaSessionConnectorModule.class,
         MediaSessionCompatModule.class,
         PlaybackManagerModule.class,
