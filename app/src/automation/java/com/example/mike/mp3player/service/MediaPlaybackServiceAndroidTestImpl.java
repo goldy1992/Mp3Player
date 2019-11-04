22d79ef3e5970571c1f754c0b5fdfd09de1a09f1
Mike Goldsmith <michael.goldsmith@datalex.com>
2019-11-04 19:09:40 +0000

    Issue-95 - removed unecessary permission from android test activity

--- app/src/automation/java/com/example/mike/mp3player/service/MediaPlaybackServiceAndroidTestImpl.java
+++ app/src/automation/java/com/example/mike/mp3player/service/MediaPlaybackServiceAndroidTestImpl.java
@@ -2,17 +2,8 @@ package com.example.mike.mp3player.service;
 
 import com.example.mike.mp3player.dagger.components.AndroidTestServiceComponent;
 import com.example.mike.mp3player.dagger.components.DaggerAndroidTestServiceComponent;
-import androidx.test.rule.GrantPermissionRule;
-
-
-import org.junit.Rule;
 
 public class MediaPlaybackServiceAndroidTestImpl extends MediaPlaybackService {
-
-    @Rule
-    public GrantPermissionRule mGrantPermissionRule =
-            GrantPermissionRule.grant(
-                    "android.permission.WRITE_EXTERNAL_STORAGE");
 
     @Override
     public void onCreate() {
