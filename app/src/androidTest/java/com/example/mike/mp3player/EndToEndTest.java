c7807e6a2413a8f2d4d6f04f73ca5fc05fde16e2
Mike Goldsmith <michael.goldsmith@datalex.com>
2019-11-05 14:18:08 +0000

    Issue-95 - added main activity tab layout assertions

--- app/src/androidTest/java/com/example/mike/mp3player/EndToEndTest.java
+++ app/src/androidTest/java/com/example/mike/mp3player/EndToEndTest.java
@@ -8,37 +8,26 @@ import android.view.ViewParent;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.NoMatchingViewException;
-import androidx.test.espresso.ViewAssertion;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
 
+import com.example.mike.mp3player.client.activities.MainActivity;
 import com.example.mike.mp3player.client.activities.MainActivityInjectorAndroidTestImpl;
+import com.example.mike.mp3player.commons.MediaItemType;
 import com.google.android.material.tabs.TabLayout;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.example.mike.mp3player.R.id.tabs;
-import static org.hamcrest.core.AllOf.allOf;
-import static org.junit.Assert.assertTrue;
+import static com.example.mike.mp3player.TestUtils.assertTabName;
+import static org.junit.Assert.assertEquals;
 
 @LargeTest
 @RunWith(androidx.test.ext.junit.runners.AndroidJUnit4.class)
@@ -47,7 +36,6 @@ public class EndToEndTest {
     IdlingResource idlingResource;
     @Rule
     public ActivityTestRule<MainActivityInjectorAndroidTestImpl> mActivityTestRule = new ActivityTestRule<>(MainActivityInjectorAndroidTestImpl.class);
-
 
     @Rule
     public GrantPermissionRule mGrantPermissionRule =
@@ -60,25 +48,24 @@ public class EndToEndTest {
         IdlingRegistry.getInstance().register(idlingResource);
     }
 
+    @After
+    public void tearDown() {
+        IdlingRegistry.getInstance().unregister(idlingResource);
+    }
+
     @Test
     public void splashScreenEntryActivityTest() {
         Espresso.onIdle();
-       // mActivityTestRule.getActivity().
-       // mActivityTestRule.getScenario();
-        onView(withId(R.id.tabs)).check(new ViewAssertion() {
-            @Override
-            public void check(View view, NoMatchingViewException noViewFoundException) {
-                if (view instanceof TabLayout) {
-                    TabLayout tabLayout = (TabLayout) view;
-                    TabLayout.Tab tab = tabLayout.getTabAt(0);
-                    assertTrue(tab.getText().equals("Songs"));
-                }
-                else
-                {
-                    throw noViewFoundException;
-                }
-            }
-        });
+        MainActivity mainActivity = mActivityTestRule.getActivity();
+        TabLayout tabLayout = mainActivity.findViewById(R.id.tabs);
+
+        final int expectedTabCount = 2;
+        final int actualTabCount = tabLayout.getTabCount();
+        assertEquals(expectedTabCount, actualTabCount);
+
+        assertTabName(tabLayout, 0, MediaItemType.SONGS.getTitle());
+        assertTabName(tabLayout, 1, MediaItemType.FOLDERS.getTitle());
+
     }
 
     private static Matcher<View> childAtPosition(
--- app/src/androidTest/java/com/example/mike/mp3player/TestUtils.java
+++ app/src/androidTest/java/com/example/mike/mp3player/TestUtils.java
@@ -1,9 +1,17 @@
 package com.example.mike.mp3player;
 
+import com.google.android.material.tabs.TabLayout;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * This class will be used for utility methods in the automation framework.
  */
 public final class TestUtils {
 
-
+    public static void assertTabName(TabLayout tabLayout, int position, String expectedTabTitle) {
+        TabLayout.Tab tab = tabLayout.getTabAt(position);
+        final String actualNameFirstTab = tab.getText().toString();
+        assertEquals(expectedTabTitle, actualNameFirstTab);
+    }
 }
