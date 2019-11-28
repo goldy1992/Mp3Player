8e4bcc92f22c0865495c6777b9a2501600b18a07
Mike Goldsmith <michael.goldsmith@datalex.com>
2019-11-28 22:23:03 +0000

    Issue-110 - added MediaItemFromIdRetrieverTest

--- app/src/main/java/com/github/goldy1992/mp3player/service/library/content/retriever/MediaItemFromIdRetriever.java
+++ app/src/main/java/com/github/goldy1992/mp3player/service/library/content/retriever/MediaItemFromIdRetriever.java
@@ -1,13 +1,12 @@
 package com.github.goldy1992.mp3player.service.library.content.retriever;
 
 import android.content.ContentResolver;
-import android.content.Context;
 import android.database.Cursor;
-import android.media.MediaMetadataRetriever;
 import android.provider.MediaStore;
 import android.support.v4.media.MediaBrowserCompat;
 
-import com.github.goldy1992.mp3player.service.library.MediaItemTypeIds;
+import androidx.annotation.Nullable;
+
 import com.github.goldy1992.mp3player.service.library.content.parser.SongResultsParser;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -26,22 +25,19 @@ public class MediaItemFromIdRetriever {
     private final SongResultsParser songResultsParser;
 
     @Inject
-    public MediaItemFromIdRetriever(Context context,
-                                ContentResolver contentResolver,
-                                SongResultsParser resultsParser,
-                                MediaMetadataRetriever mediaMetadataRetriever,
-                                MediaItemTypeIds mediaItemTypeIds) {
+    public MediaItemFromIdRetriever(ContentResolver contentResolver,
+                                SongResultsParser resultsParser) {
         this.contentResolver = contentResolver;
         this.songResultsParser = resultsParser;
     }
 
+    @Nullable
     public MediaBrowserCompat.MediaItem getItem(long id) {
         final String WHERE = MediaStore.Audio.Media._ID + " = ?";
         final String[] WHERE_ARGS = {String.valueOf(id)};
 
         Cursor cursor =  contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
                 PROJECTION, WHERE, WHERE_ARGS, null);
-
 
         if (null != cursor) {
             List<MediaBrowserCompat.MediaItem> results = songResultsParser.create(cursor, "");
