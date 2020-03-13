01b8eb967741be5b662b9de9ddc85239dc6c265b
Mike Goldsmith <michael.goldsmith@datalex.com>
2020-03-13 14:26:07 +0000

    Issue-120 - fixed compilation errors on unit tests

--- service/src/unitTest/java/com/github/goldy1992/mp3player/service/dagger/components/TestServiceComponent.kt
+++ service/src/unitTest/java/com/github/goldy1992/mp3player/service/dagger/components/TestServiceComponent.kt
@@ -1,15 +1,19 @@
 package com.github.goldy1992.mp3player.service.dagger.components
 
 import com.github.goldy1992.mp3player.service.dagger.modules.MockSearchDatabaseModule
-import com.github.goldy1992.mp3player.service.dagger.modules.service.ContentManagerModule
-import com.github.goldy1992.mp3player.service.dagger.modules.service.ExoPlayerModule
-import com.github.goldy1992.mp3player.service.dagger.modules.service.MediaSessionCompatModule
-import com.github.goldy1992.mp3player.service.dagger.modules.service.MediaSessionConnectorModule
+import com.github.goldy1992.mp3player.service.dagger.modules.service.*
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ContentManagerModule::class, ExoPlayerModule::class, MediaSessionConnectorModule::class, MediaSessionCompatModule::class, MockSearchDatabaseModule::class])
+@Component(modules = [
+    ContentManagerModule::class,
+    ContentRetrieversModule::class,
+    ContentSearchersModule::class,
+    ExoPlayerModule::class,
+    MediaSessionConnectorModule::class,
+    MediaSessionCompatModule::class,
+    MockSearchDatabaseModule::class])
 interface TestServiceComponent : ServiceComponent {
     @Component.Factory
     interface Factory : ServiceComponent.Factory
