e0fc921378ea1c31de758c32b1f825a336943dfb
Mike <goldy13_1992@hotmail.co.uk>
2020-08-13 16:19:28 +0100

    Issue-130 - Fixed keyboard issues in SearchResultsFragment.kt

--- client/src/main/java/com/github/goldy1992/mp3player/client/views/fragments/SearchResultsFragment.kt
+++ client/src/main/java/com/github/goldy1992/mp3player/client/views/fragments/SearchResultsFragment.kt
@@ -5,13 +5,21 @@ import android.app.SearchManager
 import android.content.ComponentName
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.ResultReceiver
 import android.support.v4.media.MediaBrowserCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
+import android.view.inputmethod.InputMethodManager.SHOW_FORCED
+import android.view.inputmethod.InputMethodManager.SHOW_IMPLICIT
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.SearchView
@@ -30,8 +38,10 @@ import com.github.goldy1992.mp3player.commons.LogTagger
 import com.github.goldy1992.mp3player.commons.MediaItemType
 import com.github.goldy1992.mp3player.commons.MediaItemUtils
 import dagger.hilt.android.AndroidEntryPoint
+import net.yslibrary.android.keyboardvisibilityevent.util.UIUtil
 import org.apache.commons.lang3.StringUtils.isNotEmpty
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SearchResultsFragment : DestinationFragment(), MyGenericItemTouchListener.ItemSelectedListener, LogTagger {
@@ -51,11 +61,16 @@ class SearchResultsFragment : DestinationFragment(), MyGenericItemTouchListener.
 
     private val viewModel : SearchResultsFragmentViewModel by viewModels()
 
+    private val resultReceiver : MyResultReceiver = MyResultReceiver()
+
+    private lateinit var searchView: SearchView
+
     override fun lockDrawerLayout(): Boolean {
         return false
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        this.inputMethodManager = requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         setHasOptionsMenu(true)
         val binding = FragmentSearchResultsBinding.inflate(inflater)
         val recyclerView = binding.recyclerView
@@ -65,29 +80,18 @@ class SearchResultsFragment : DestinationFragment(), MyGenericItemTouchListener.
         itemTouchListener.parentView = recyclerView
         val context : Context = requireContext()
         recyclerView.layoutManager = LinearLayoutManager(context)
-        // Get the SearchView and set the searchable configuration
-        val searchManager = context.getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val componentName = ComponentName(context, requireActivity().javaClass)
-        val searchableInfo = searchManager.getSearchableInfo(componentName)
-        //    Assumes current activity is the searchable activity
+        // Assumes current activity is the searchable activity
         setUpToolbar(binding.toolbar)
-//        val searchView = binding.searchView
-//         searchView.setOnQueryTextListener(queryListener)
-//        searchView.setSearchableInfo(searchableInfo)
-//        //searchView.set
-//        searchView.setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
-//        searchView.isSubmitButtonEnabled = true
-//        searchView.setBackgroundColor(Color.WHITE)
-//        searchView.setOnQueryTextFocusChangeListener(View.OnFocusChangeListener { _: View?, queryTextFocused: Boolean -> onFocusChange(queryTextFocused) })
 
         subscribeUi(searchResultAdapter)
 
-        inputMethodManager = requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     override fun itemSelected(item: MediaBrowserCompat.MediaItem?) {
         if (null != item) {
@@ -112,25 +116,35 @@ class SearchResultsFragment : DestinationFragment(), MyGenericItemTouchListener.
         super.onCreateOptionsMenu(menu, inflater)
     }
 
-//    @VisibleForTesting
-//    fun onFocusChange(queryTextFocused: Boolean) {
-//        Log.i("tag", "focus changed: has focus: $queryTextFocused")
-//        if (!queryTextFocused) {
-//            val fragmentManager = parentFragmentManager
-//            if (!fragmentManager.isDestroyed) {
-//                fragmentManager.popBackStack()
-//            }
-//        }
-//    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(logTag(), "on pause")
+        hideKeyboard()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(logTag(), "on resume")
+        showKeyboard()
+    }
 
     private fun configureSearchView(searchMenu: Menu) {
         val item = searchMenu.findItem(R.id.search)
         val searchView = item.actionView as SearchView
-        searchView.setIconifiedByDefault(false)
         searchView.queryHint = "Search Media"
         val magId = resources.getIdentifier("android:id/search_mag_icon", null, null)
         val magImage = searchView.findViewById<View>(magId) as ImageView
         magImage.layoutParams = LinearLayout.LayoutParams(0, 0)
+
+        // Get the SearchView and set the searchable configuration
+        val searchManager = requireContext().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val componentName = ComponentName(context, requireActivity().javaClass)
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        searchView.setOnQueryTextListener(queryListener)
+        searchView.setSearchableInfo(searchableInfo)
+        searchView.setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+        searchView.isSubmitButtonEnabled = true
+        searchView.requestFocus()
     }
 
     inner class QueryListener : SearchView.OnQueryTextListener {
@@ -164,5 +178,32 @@ class SearchResultsFragment : DestinationFragment(), MyGenericItemTouchListener.
      */
     override fun logTag(): String {
         return "SRCH_RSLTS_FRGMNT"
+    }
+
+
+    private fun hideKeyboard() {
+        try {
+            val activity : Activity = requireActivity()
+            //activity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+            val view : View? = activity.currentFocus
+            if (view?.windowToken != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0, resultReceiver)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun showKeyboard() {
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, HIDE_IMPLICIT_ONLY)
+    }
+
+    inner class MyResultReceiver : ResultReceiver(Handler(Looper.getMainLooper())) {
+
+        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+            super.onReceiveResult(resultCode, resultData)
+            Log.i("ResultReceiver", "resHandler: $resultCode")
+        }
+
     }
 }
