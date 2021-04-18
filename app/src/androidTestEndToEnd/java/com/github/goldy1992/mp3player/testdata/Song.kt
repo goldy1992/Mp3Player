8a4ba1693e14b290d230060c3e2b6b6e4447917d
Michael Goldsmith <michael.goldsmith@datalex.com>
2021-04-18 21:17:30 +0100

    fix(develop-migration): migrated develop changes to master branch

--- app/src/androidTestEndToEnd/java/com/github/goldy1992/mp3player/testdata/Song.kt
+++
@@ -1,3 +0,0 @@
-package com.github.goldy1992.mp3player.testdata
-
-class Song constructor(val title : String, val artist : String)
--- client/src/testUnitTest/java/com/github/goldy1992/mp3player/client/views/MyAttributeSetBuilderImpl.kt
+++
@@ -1,5 +0,0 @@
-package com.github.goldy1992.mp3player.client.views
-
-import org.robolectric.android.AttributeSetBuilderImpl
-
-class MyAttributeSetBuilderImpl(resourceResolver: ResourceResolver?) : AttributeSetBuilderImpl(resourceResolver)
