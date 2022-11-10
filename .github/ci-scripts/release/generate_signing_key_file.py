5fd5502d5d1f3fc2c563689797aa371c3020b8b5
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2022-11-10 15:03:25 +0000

    Tried to use os.path.join to resolve directory issue

--- .github/ci-scripts/release/generate_signing_key_file.py
+++ .github/ci-scripts/release/generate_signing_key_file.py
@@ -1,11 +1,12 @@
 import sys
+import os
 
 signing_key_file_path = sys.argv[1]
 base_64_key = sys.argv[2]
 
 print(f'signing key file path: {signing_key_file_path}')
 
-with open(signing_key_file_path, "w", encoding="utf-8") as f:
+with open(os.path.join(os.getcwd(), signing_key_file_path), "w", encoding="utf-8") as f:
     print("file created and opened successfully")
     f.write(base_64_key)
 
