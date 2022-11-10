dfbeb00e8db15d9e295b640a42cad426d8c0d158
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2022-11-10 14:08:59 +0000

    separated signing key file tasks

--- .github/ci-scripts/release/generate_signing_key_file.py
+++ .github/ci-scripts/release/generate_signing_key_file.py
@@ -3,7 +3,7 @@ import sys
 signing_key_file_path = sys.argv[1]
 base_64_key = sys.argv[2]
 
-with open(signing_key_file_path, "w+",encoding="utf-8") as f:
+with open(signing_key_file_path, "w+", encoding="utf-8") as f:
     f.write(base_64_key)
 
 f.closed
