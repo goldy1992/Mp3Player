00920bfdade44e18ed25bd0c73e6243be1c46a13
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2022-11-10 14:53:52 +0000

    Try to use python3

--- .github/ci-scripts/release/generate_signing_key_file.py
+++ .github/ci-scripts/release/generate_signing_key_file.py
@@ -6,6 +6,7 @@ base_64_key = sys.argv[2]
 print(f'signing key file path: {signing_key_file_path}')
 
 with open(signing_key_file_path, "w", encoding="utf-8") as f:
+    print("file created and opened successfully")
     f.write(base_64_key)
 
 f.closed
