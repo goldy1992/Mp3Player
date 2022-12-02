e73d60756147ad9cd48c7d41532b45d4ab50e685
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2022-12-02 15:36:19 +0000

    equalizer - Fixed performance issues in the firework equalizer

--- client/src/main/java/com/github/goldy1992/mp3player/client/ui/components/equalizer/fireworks/FireworkEqualizerNew.kt
+++ client/src/main/java/com/github/goldy1992/mp3player/client/ui/components/equalizer/fireworks/FireworkEqualizerNew.kt
@@ -6,10 +6,7 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -21,6 +18,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.github.goldy1992.mp3player.client.ui.screens.DpPxSize
 import kotlinx.coroutines.android.awaitFrame
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import org.apache.commons.collections4.CollectionUtils.isNotEmpty
 import kotlin.math.*
 import kotlin.random.Random
@@ -50,44 +49,72 @@ fun FireworkWrapperNew(modifier: Modifier = Modifier,
         ) }
    // Log.i(logTag, "spawnPoints: $spawnPoints")
 
-    val particles : SnapshotStateMap<Int, List<Particle>> = remember(spawnPoints) {
+    var particles : Map<Int, List<Particle>> by remember(spawnPoints) {
         Log.i(logTag, "new particles map")
-        val map : SnapshotStateMap<Int, List<Particle>> = mutableStateMapOf()
+        val map : MutableMap<Int, List<Particle>> = mutableMapOf()
         spawnPoints.indices.forEach { idx -> map[idx] = listOf() }
-        map
+        mutableStateOf(map.toMap())
     }
 
     // TODO: separate into 2 separate flows, 1. add new particles 2. update animation
-    LaunchedEffect(frequencyPhases, particles) {
-        val frame = awaitFrame()
-        val newParticleMap : MutableMap<Int, List<Particle>> = mutableMapOf()
-        particles.entries.forEach { entry ->
-            val newList : MutableList<Particle> = mutableListOf()
-            for (v in entry.value) {
-                if (v.y < (canvasSize.heightPx + 10)) {
-                    newList.add(updateParticle(v, frame))
-                }
-            }
-            if (newList.size < 15) {
-                Log.i(logTag, "adding new particle to freq: ${entry.key}")
-                val currentFreq = frequencyPhases[entry.key]
-                val frac = currentFreq / MAX_FREQUENCY
-                val hmax = canvasSize.heightPx * frac
-                val angle = Math.toRadians(Random.nextDouble(85.0, 95.0))
-                val initialVelocity = calculateInitialVelocityGivenHmax(hmax, angle)
+    LaunchedEffect(frequencyPhases) {
+        this.launch {
+                Log.i(logTag, "frequencyPhases updated, new launchedeffect")
+            val entryIterator = particles.entries.iterator()
+            val newMap = mutableMapOf<Int, List<Particle>>()
+            while (entryIterator.hasNext()) {
+                val entry = entryIterator.next()
+                val currentParticleList = entry.value
+                val newParticleList = currentParticleList.toMutableList()
+                if (newParticleList.size < 15) {
+//                    Log.i(logTag, "adding new particle to freq: ${entry.key}")
+                    val currentFreq = frequencyPhases[entry.key]
+                    val frac = currentFreq / MAX_FREQUENCY
+                    val hmax = canvasSize.heightPx * frac
+                    val angle = Math.toRadians(Random.nextDouble(85.0, 95.0))
+                    val initialVelocity = calculateInitialVelocityGivenHmax(hmax, angle)
 
-                newList.add(
-                    createParticle(
-                        spawnPoints[entry.key], initialVelocity = initialVelocity,
-                        color = if (entry.key % 2 == 0) Color.Blue else particleColor
+                    newParticleList.add(
+                        createParticle(
+                            spawnPoints[entry.key], initialVelocity = initialVelocity,
+                            color = if (entry.key % 2 == 0) Color.Blue else particleColor
+                        )
+
                     )
-
-                )
+                }
+                newMap[entry.key] = newParticleList.toList()
             }
-            newParticleMap[entry.key] = newList.toList() ?: emptyList()
+            particles = newMap
+            Log.i(logTag, "set new map after creating new particles")
         }
-        for (i in particles.entries) {
-            particles.replace(i.key, newParticleMap[i.key] ?: emptyList())
+    }
+
+
+    LaunchedEffect(particles) {
+        this.launch {
+            Log.i(logTag, "launching new while particles in map corouting")
+            while (particlesInMap(particles)) {
+                Log.i(logTag, "while Particles in map, update particles")
+                val frame = awaitFrame()
+                val entryIterator = particles.entries.iterator()
+                val newMap = mutableMapOf<Int, List<Particle>>()
+                while (entryIterator.hasNext()) {
+                    val entry = entryIterator.next()
+                    val newList : MutableList<Particle> = mutableListOf()
+                    val valueIterator = entry.value.iterator()
+                    while (valueIterator.hasNext()) {
+                        val currentParticle = valueIterator.next()
+                        if (currentParticle.y < (canvasSize.heightPx + 10)) {
+                            Log.i(logTag, "updatingparticle")
+                            newList.add(updateParticle(currentParticle, frame))
+                        }
+                    }
+                    newMap[entry.key] = newList.toList()
+                }
+                particles = newMap
+                Log.i(logTag, "set new map after updating particles")
+
+            }
         }
     }
 
@@ -108,11 +135,11 @@ private fun FireworkEqualizer(
     modifier: Modifier = Modifier,
     surfaceColor : Color = MaterialTheme.colorScheme.primaryContainer
 ) {
-    var msg = "equalizer called with: "
-    for (p in particles.entries) {
-        msg += "${p.key}: ${p.value}\n"
-    }
-    Log.i(logTag, msg)
+//    var msg = "equalizer called with: "
+//    for (p in particles.entries) {
+//        msg += "${p.key}: ${p.value}\n"
+//    }
+//    Log.i(logTag, msg)
     Canvas( modifier = modifier
         .fillMaxSize()
     ) {
@@ -244,14 +271,17 @@ private fun calculateInitialVelocityGivenHmax(hmax : Float, angle : Double) : Fl
 
 private fun particlesInMap(particleMap : Map<Int, List<Particle>>) : Boolean {
     if (particleMap.isEmpty()) {
+        Log.i(logTag, "particles map empty, returning false")
         return false
     }
 
     particleMap.entries.forEach { entry ->
         if (isNotEmpty(entry.value)) {
+            Log.i(logTag, "non-empty list present,, returning true")
             return true
         }
     }
+    Log.i(logTag, "defaulting to particlesInMap: false")
     return false
 }
 
