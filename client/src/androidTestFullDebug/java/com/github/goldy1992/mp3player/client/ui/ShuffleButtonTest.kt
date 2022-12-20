b4b4ebf2a3ce3ebc7f42a77e91c670cd89f25b19
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2022-12-20 05:08:58 +0000

    equalizer - Fixed the Media Buttons tests

--- client/src/androidTestFullDebug/java/com/github/goldy1992/mp3player/client/ui/ShuffleButtonTest.kt
+++ client/src/androidTestFullDebug/java/com/github/goldy1992/mp3player/client/ui/ShuffleButtonTest.kt
@@ -6,79 +6,77 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
-import com.github.goldy1992.mp3player.client.MediaControllerAdapter
 import com.github.goldy1992.mp3player.client.R
-import com.github.goldy1992.mp3player.client.ui.flows.player.ShuffleModeFlow
 import com.github.goldy1992.mp3player.client.ui.buttons.ShuffleButton
 import com.github.goldy1992.mp3player.client.ui.buttons.ShuffleOffButton
 import com.github.goldy1992.mp3player.client.ui.buttons.ShuffleOnButton
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 /**
  * Test class for the [ShuffleButton]
  */
 class ShuffleButtonTest {
-    @Mock
-    val mockMediaController = mock<MediaControllerAdapter>()
 
-    val shuffleModeFlow = mock<ShuffleModeFlow>()
+    private val mockOnClick = mock<MockOnClick>()
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
     /**
-     * Tests that when the state of the [MediaControllerAdapter] says that Shuffle Mode is on, then
-     * the [ShuffleOffButton] should be displayed.
-     * When the [ShuffleOffButton] is clicked, [MediaControllerAdapter.setShuffleMode] should be
-     * called with [PlaybackStateCompat.SHUFFLE_MODE_ALL]
+     * WHEN: the shuffle mode is DISABLED
+     * THEN: the [ShuffleOffButton] should be displayed.
+     * AND WHEN: the [ShuffleOffButton] is clicked,
+     * THEN: The onClick function should be called to ENABLE shuffle
      */
     @Test
     fun testShuffleOffButtonDisplayedWhenShuffleModeOff() {
         val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
         val expected = context.resources.getString(R.string.shuffle_off)
+        val shuffleEnabled = false
         // Set Shuffle Mode to be Off
-       // whenever(shuffleModeFlow.state).thenReturn(MutableStateFlow(false))
         composeTestRule.setContent {
-            ShuffleButton(mediaController = mockMediaController, shuffleModeState = MutableStateFlow(false))
+            ShuffleButton(
+                shuffleEnabledProvider = { shuffleEnabled },
+                onClick = {mockOnClick.onClick(it) }
+            )
         }
         composeTestRule.onNodeWithContentDescription(expected, useUnmergedTree = true).assertExists()
         val shuffleOffButton = composeTestRule.onNode(hasContentDescription(expected), useUnmergedTree = true)
         shuffleOffButton.assertExists()
         shuffleOffButton.performClick()
-        runBlocking {
-            composeTestRule.awaitIdle()
-           // verify(mockMediaController, times(1)).setShuffleMode(SHUFFLE_ON)
-        }
+        verify(mockOnClick).onClick(true)
 
     }
 
     /**
-     * Tests that when the state of the [MediaControllerAdapter] says that Shuffle Mode is on, then
-     * the [ShuffleOnButton] should be displayed.
-     * When the [ShuffleOnButton] is clicked, [MediaControllerAdapter.setShuffleMode] should be
-     * called with [PlaybackStateCompat.SHUFFLE_MODE_NONE]
+     * WHEN: the shuffle mode is ENABLED
+     * THEN: the [ShuffleOnButton] should be displayed.
+     * AND WHEN: the [ShuffleOnButton] is clicked,
+     * THEN: The onClick function should be called to DISABLE shuffle
      */
     @Test
     fun testShuffleOnButtonDisplayedWhenShuffleModeOn() {
         val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
         val expected = context.resources.getString(R.string.shuffle_on)
         // Set Shuffle Mode to be On
-     //   whenever(mockMediaController.shuffleMode).thenReturn(MutableLiveData(SHUFFLE_ON))
+        val shuffleEnabled = true
         composeTestRule.setContent {
-            ShuffleButton(mediaController = mockMediaController, shuffleModeState = MutableStateFlow(true))
+            ShuffleButton(
+                shuffleEnabledProvider = { shuffleEnabled },
+                onClick = { mockOnClick.onClick(it) }
+            )
         }
         composeTestRule.onNodeWithContentDescription(expected, useUnmergedTree = true).assertExists()
         val shuffleOnButton = composeTestRule.onNode(hasContentDescription(expected), useUnmergedTree = true)
         shuffleOnButton.assertExists()
         shuffleOnButton.performClick()
-        runBlocking {
-            composeTestRule.awaitIdle()
-  //          verify(mockMediaController, times(1)).setShuffleMode(SHUFFLE_OFF)
-        }
+        verify(mockOnClick).onClick(false)
+    }
+
+    private class MockOnClick {
+        fun onClick(shuffleEnabled : Boolean) { }
     }
 }
--- client/src/main/java/com/github/goldy1992/mp3player/client/ui/buttons/MediaButtons.kt
+++ client/src/main/java/com/github/goldy1992/mp3player/client/ui/buttons/MediaButtons.kt
@@ -7,41 +7,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import com.github.goldy1992.mp3player.client.MediaControllerAdapter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-
-@Composable
-fun SkipToPreviousButton(mediaController: MediaControllerAdapter,
-scope : CoroutineScope = rememberCoroutineScope()) {
-    IconButton(onClick = {
-        scope.launch { mediaController.skipToPrevious() }}) {
-        Icon(
-            Icons.Filled.SkipPrevious,
-            "Skip to Previous",
-            tint = MaterialTheme.colorScheme.primary
-        )
-    }
-}
 
 @Composable
 fun SkipToPreviousButton(onClick : () -> Unit = {} ) {
     IconButton(onClick = { onClick() }){
         Icon(
             Icons.Filled.SkipPrevious,
-            "Skip to Previous",
-            tint = MaterialTheme.colorScheme.primary
-        )
-    }
-}
-
-@Composable
-fun SkipToNextButton(mediaController: MediaControllerAdapter,
-scope: CoroutineScope = rememberCoroutineScope()) {
-    IconButton(onClick = { scope.launch { mediaController.skipToNext()}}) {
-        Icon(
-            Icons.Filled.SkipNext,
             "Skip to Previous",
             tint = MaterialTheme.colorScheme.primary
         )
