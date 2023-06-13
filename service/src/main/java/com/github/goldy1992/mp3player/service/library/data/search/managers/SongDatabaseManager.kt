06a58707c68e71c85be190c081d847bfbff6895b
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2023-06-13 05:14:25 +0100

    updated gradlew execute permissions

--- service/src/main/java/com/github/goldy1992/mp3player/service/library/data/search/managers/SongDatabaseManager.kt
+++ service/src/main/java/com/github/goldy1992/mp3player/service/library/data/search/managers/SongDatabaseManager.kt
@@ -1,20 +1,16 @@
 package com.github.goldy1992.mp3player.service.library.data.search.managers
 
 import androidx.media3.common.MediaItem
-import com.github.goldy1992.mp3player.commons.MediaItemType
 import com.github.goldy1992.mp3player.commons.MediaItemUtils.getMediaId
 import com.github.goldy1992.mp3player.commons.MediaItemUtils.getTitle
 import com.github.goldy1992.mp3player.commons.Normaliser.normalise
 import com.github.goldy1992.mp3player.service.library.ContentManager
-import com.github.goldy1992.mp3player.service.library.MediaItemTypeIds
 import com.github.goldy1992.mp3player.service.library.data.search.SearchDatabase
 import com.github.goldy1992.mp3player.service.library.data.search.Song
-import dagger.hilt.android.scopes.ServiceScoped
-import javax.inject.Inject
 
-@ServiceScoped
+
 class SongDatabaseManager
-    @Inject
+
     constructor(contentManager: ContentManager,
                 rootItemId : String,
                 searchDatabase: SearchDatabase
