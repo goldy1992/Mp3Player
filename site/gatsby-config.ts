77485620f0d7027781e232d0411035b21d4ad54d
Mike <goldy13_1992@hotmail.co.uk>
2024-04-08 08:16:55 +0700

    streamlined gatsby plugins

--- site/gatsby-config.ts
+++ site/gatsby-config.ts
@@ -3,42 +3,52 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `website`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `MP3 Player`,
+    siteUrl: `https://goldy1992.github.io/Mp3Player/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-root-import", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-tsconfig-paths", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  },
-  {
-    resolve: "gatsby-plugin-react-svg",
-    options: {
-      rule: {
-        include: /assets/ // See below to configure properly
+  plugins: [
+    "gatsby-plugin-root-import", 
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+    {
+          resolve: 'gatsby-source-filesystem',
+          options: {
+            "name": "images",
+            "path": "./src/images/"
+          },
+          __key: "images"
+        }, {
+          resolve: 'gatsby-source-filesystem',
+          options: {
+            "name": "pages",
+            "path": "./src/pages/"
+          },
+          __key: "pages"
+        },
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `MP3 Player Webpage`,
+        short_name: `MP3 Player`,
+        start_url: `/Mp3Player`,
+        icon: `src/assets/logo.svg`,
+      },
+    }, 
+    "gatsby-plugin-tsconfig-paths",
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/ // See below to configure properly
+        }
       }
-    }
-  },
-
+    },
 
 ],
 
