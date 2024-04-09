10e56f85d1f90f8c15be70c54c5ccc0c886e6da2
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2024-04-09 03:29:52 +0100

    Fixed text alignment on homepage

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -21,8 +21,11 @@ const Home: React.FC = () => {
         <AppBar />
       </div>
       <div className="flex flex-col flex-grow items-center justify-between px-24 pt-10 pb-[10px]">
-        <div className="flex-1 flex items-center text-center">An <span className="font-bold italic">open source</span> <span className="text-3xl font-extrabold text-nowrap">MP3 Player</span> for <span className="font-bold">Android</span></div>
-
+        
+        <div className="flex-1 flex items-center text-center">
+          <div>An <span className="font-bold italic">open source</span> <span className="text-3xl font-extrabold text-nowrap">MP3 Player</span> for <span className="font-bold">Android</span>
+          </div>
+        </div>
         <div className="flex-1 flex items-center"><GithubIcon /></div>
         <div className="flex flex items-center flex-1 items-center space-x-4 text-sm">
           <div>© 2024 <a className="hover:cursor-pointer" href="https://github.com/goldy1992">goldy1992</a></div>
