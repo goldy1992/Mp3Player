9d2603223f9a2b506e2823c5b14aae3f2ca5bd1c
Michael Goldsmith <goldy13_1992@hotmail.co.uk>
2025-09-07 08:27:59 +0700

    fix(cicd): updated the CICD scripts as well as upgrade to AGP 8.13.0

--- app/src/main/java/com/github/goldy1992/mp3player/MainApplication.kt
+++ app/src/main/java/com/github/goldy1992/mp3player/MainApplication.kt
@@ -2,22 +2,8 @@ package com.github.goldy1992.mp3player
 
 
 import android.app.Application
-import com.github.goldy1992.mp3player.commons.SingletonCoroutineScope
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
-import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication : Application()
 
-    @SingletonCoroutineScope
-    @Inject
-    lateinit var scope : CoroutineScope
-
-    override fun onTerminate() {
-        super.onTerminate()
-        scope.cancel()
-    }
-
-}
